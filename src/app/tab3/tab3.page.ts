5baf6451d03925bcc4d3ea5e71ae830ae15502b2
Aaron Bujatin <aaronbujatin@gmail.com>
2023-09-25 18:24:37 +0800

    modal added

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { VendorService } from '../service/vendor.service';
 import { Vendor } from '../model/vendor.model';
-
+import { IonModal } from '@ionic/angular';
+import { OverlayEventDetail } from '@ionic/core/components';
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
@@ -23,6 +24,26 @@ export class Tab3Page {
         
       }
     )
+  }
+
+  @ViewChild(IonModal) modal: IonModal;
+
+  message = 'This modal example uses triggers to automatically open a modal when the button is clicked.';
+  name: string;
+
+  cancel() {
+    this.modal.dismiss(null, 'cancel');
+  }
+
+  confirm() {
+    this.modal.dismiss(this.name, 'confirm');
+  }
+
+  onWillDismiss(event: Event) {
+    const ev = event as CustomEvent<OverlayEventDetail<string>>;
+    if (ev.detail.role === 'confirm') {
+      this.message = `Hello, ${ev.detail.data}!`;
+    }
   }
 
 
