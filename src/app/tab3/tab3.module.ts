0aa17a9be53918582df748230e1010b63a9aab3b
Aaron Bujatin <aaronbujatin@gmail.com>
2023-09-26 10:55:47 +0800

    vendor form configuring

--- src/app/tab3/tab3.module.ts
+++ src/app/tab3/tab3.module.ts
@@ -1,7 +1,7 @@
 import { IonicModule } from '@ionic/angular';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Tab3Page } from './tab3.page';
 
 import { Tab3PageRoutingModule } from './tab3-routing.module';
@@ -11,7 +11,8 @@ import { Tab3PageRoutingModule } from './tab3-routing.module';
     IonicModule,
     CommonModule,
     FormsModule,
-    Tab3PageRoutingModule
+    Tab3PageRoutingModule,
+    ReactiveFormsModule
   ],
   declarations: [Tab3Page]
 })
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -3,25 +3,58 @@ import { VendorService } from '../service/vendor.service';
 import { Vendor } from '../model/vendor.model';
 import { IonModal } from '@ionic/angular';
 import { OverlayEventDetail } from '@ionic/core/components';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';;
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
+  ionicForm: any;
+  submitForm() {
+    throw new Error('Method not implemented.');
+  }
 
-  constructor(private vendorService : VendorService) {}
+  constructor(private vendorService: VendorService, private formBuilder: FormBuilder) { }
 
-  vendors : Vendor[];
 
-  ngOnInit(){
+  vendorForm: FormGroup
+  vendors: Vendor[];
+
+  ngOnInit() {
+    this.getAllVendors();
+  }
+
+  public initForm() {
+    this.vendorForm = this.formBuilder.group({
+      eventNameReference: ['', [Validators.required]],
+      vendorType: ['', [Validators.required]],
+      companyName: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,3}$'),],],
+      regionCityZip: ['', [Validators.required]],
+      contact: ['', [Validators.required], Validators.pattern('^[0-9]+$')],
+      website: ['', [Validators.required]],
+      contractSigned: ['', [Validators.required]],
+      contractExpired: ['', [Validators.required]],
+      emailAddressLine: ['', [Validators.required]],
+      paymentStatus: ['', [Validators.required]],
+      finalCost: ['', [Validators.required]],
+      contractDescription: ['', [Validators.required]],
+    })
+  }
+
+  get errorControl() {
+    return this.ionicForm.controls;
+  }
+
+  public getAllVendors() {
     this.vendorService.getAllVendor().subscribe(
-      (response : Vendor[]) => {
+      (response: Vendor[]) => {
         this.vendors = response
         console.log(response);
       }, (error) => {
         console.log(error);
-        
+
       }
     )
   }
@@ -46,9 +79,21 @@ export class Tab3Page {
     }
   }
 
+  events = [
+    {
+      "id": 1, "name": "Black clover"
+    },
+    {
+      "id": 2, "name": "Naruto Shippuden"
+    },
+    {
+      "id": 3, "name": "Bluelock"
+    }
+  ]
 
-  
 
-  
+
+
+
 
 }
